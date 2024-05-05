9b8ecf691c00ec57d68c27ceb0cf62131f291a5c
Leonardo Campos <leonardoworld@outlook.com>
2024-05-05 11:27:39 -0300

    feat: add category product page

---
+++ app/categories/[id]/products/page.tsx
@@ -0,0 +1,54 @@
+import Header from "@/app/_components/header";
+import ProductItem from "@/app/_components/product-item";
+import { db } from "@/app/_lib/prisma";
+import { notFound } from "next/navigation";
+
+interface CategoriesPageProps {
+  params: {
+    id: string;
+  };
+}
+
+const CategoriesPage = async ({ params: { id } }: CategoriesPageProps) => {
+  const category = await db.category.findUnique({
+    where: {
+      id: id,
+    },
+    include: {
+      products: {
+        include: {
+          restaurant: {
+            select: {
+              name: true,
+            },
+          },
+        },
+      },
+    },
+  });
+
+  if (!category) {
+    return notFound();
+  }
+  return (
+    <>
+      <Header />
+      <div className="px-5 pt-6">
+        <h2 className="mb-4 text-lg font-semibold tracking-[-0.4px]">
+          {category.name}
+        </h2>
+        <div className="mb-4 grid grid-cols-2 gap-6">
+          {category.products.map((product) => (
+            <ProductItem
+              key={product.id}
+              product={product}
+              className="min-w-full"
+            />
+          ))}
+        </div>
+      </div>
+    </>
+  );
+};
+
+export default CategoriesPage;
