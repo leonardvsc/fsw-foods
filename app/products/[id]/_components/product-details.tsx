6bc50b6a96a9148b6a5a3af3232dcd11d1b8eff0
Leonardo Campos <leonardoworld@outlook.com>
2024-05-04 23:23:55 -0300

    feat: add cart button to product page

--- app/products/[id]/_components/product-details.tsx
+++ app/products/[id]/_components/product-details.tsx
@@ -172,6 +172,10 @@ const ProductDetails = ({
         <h3 className="font-semibold">Sucos</h3>
         <ProductList products={complementaryProducts} />
       </div>
+
+      <div className="mt-6 px-5">
+        <Button className="w-full font-semibold">Adicionar à sacola</Button>
+      </div>
     </div>
   );
 };
