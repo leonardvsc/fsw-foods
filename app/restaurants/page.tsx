e63162963d19924fee19d6b443d431aea99536e6
Leonardo Campos <leonardoworld@outlook.com>
2024-05-05 12:22:43 -0300

    feat: add search for restaurants page

---
+++ app/restaurants/page.tsx
@@ -0,0 +1,54 @@
+"use client";
+
+import { Restaurant } from "@prisma/client";
+import { notFound, useSearchParams } from "next/navigation";
+import { useEffect, useState } from "react";
+import { searchForRestaurant } from "./_actions/search";
+import Header from "../_components/header";
+import RestaurantItem from "../_components/restaurant-item";
+
+const Restaurants = () => {
+  const searchParams = useSearchParams();
+  const [restaurants, setRestaurants] = useState<Restaurant[]>([]);
+
+  const searchFor = searchParams.get("search");
+
+  useEffect(() => {
+    const fetchRestaurants = async () => {
+      if (!searchFor) {
+        return;
+      }
+
+      const foundRestaurents = await searchForRestaurant(searchFor);
+      setRestaurants(foundRestaurents);
+    };
+
+    fetchRestaurants();
+  }, [searchFor]);
+
+  if (!searchFor) {
+    return notFound();
+  }
+
+  return (
+    <>
+      <Header />
+      <div className="px-5 py-6">
+        <h2 className="mb-4 text-lg font-semibold tracking-[-0.4px]">
+          Restaurantes Encontrados
+        </h2>
+        <div className="flex flex-col gap-6">
+          {restaurants.map((restaurant) => (
+            <RestaurantItem
+              key={restaurant.id}
+              restaurant={restaurant}
+              className="min-w-full max-w-full"
+            />
+          ))}
+        </div>
+      </div>
+    </>
+  );
+};
+
+export default Restaurants;
